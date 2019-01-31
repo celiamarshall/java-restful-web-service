f031e1b010435833e50b8aaff693f600b470ae70
Celia Marshall <celia.m@gmail.com>
2019-01-31 10:50:00 -0800

    initial commit

---
+++ src/main/java/com/example/api/restfulwebservices/exception/UserNotCreatedException.java
@@ -0,0 +1,12 @@
+package com.example.api.restfulwebservices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserNotCreatedException extends RuntimeException {
+	public UserNotCreatedException(String message) {
+		super(message);
+	}
+
+}
